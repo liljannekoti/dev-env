faf995ac62bffcf8536695412d2d35bfa1e1ce70
perttunurmi <perttu.nurmi@gmail.com>
2025-06-19 09:50:02 +0000

    updated config

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,4 @@
 { config, lib, pkgs, ... }:
-
 # sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable && sudo nix-channel --update
 {
   imports = [
@@ -7,20 +6,24 @@
     (fetchTarball "https://github.com/nix-community/nixos-vscode-server/tarball/master")
   ];
 
-  services.vscode-server.enable = true;
-
-  networking.firewall = {
-    enable = true;
-  };
-
-  users.users.perttu = {
-    extraGroups = [ "wheel" "networkmanager" "docker" ];
-  };
+  programs.nix-ld.enable = true;
 
   wsl = {
     enable = true;
     defaultUser = "perttu";
     docker-desktop.enable = true;
+  };
+
+  # vscode server for wsl and ssh
+  services.vscode-server.enable = true;
+
+  networking.firewall = {
+    enable = false;
+  };
+
+  users.users.perttu = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "docker" ];
   };
 
   virtualisation.docker = {
@@ -44,11 +47,33 @@
 
   environment.systemPackages = with pkgs;
     [
+  (vscode-with-extensions.override {
+    vscodeExtensions = with vscode-extensions; [
+      bbenoist.nix
+      ms-python.python
+      ms-azuretools.vscode-docker
+      ms-vscode-remote.remote-ssh
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "remote-ssh-edit";
+        publisher = "ms-vscode-remote";
+        version = "0.47.2";
+        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+      }
+    ];
+  })
+      jdt-language-server
+      lua-language-server
+      vscode-js-debug
       unstable.neovim
-      vscode
+      rust-analyzer
+      texliveFull
       vscode.fhs
       zoxide
+      lombok
       docker
+      maven
+      tmux
       vim
       wget
       git
@@ -61,13 +86,16 @@
       unzip
       cargo
       ripgrep
+      bat
       fd
-      go
       eza
       fzf
       lua
       python3Full
+      gnumake
     ];
+
+  programs.java = { enable = true; };
 
   environment = {
     variables = {
@@ -78,9 +106,9 @@
   };
 
   programs.bash.shellAliases = {
-    ls = "eza --color=always --group-directories-first --git --git-repos";
+    ls = "eza --color=always --group-directories-first";
     ll = "eza -l --color=always --group-directories-first --git --git-repos";
-    la = "eza -a --color=always --group-directories-first --git --git-repos";
+    la = "eza -a --color=always --group-directories-first";
     lla = "eza -alhF --color=always --group-directories-first --git --git-repos";
   };
 
@@ -90,5 +118,5 @@
     settings.KbdInteractiveAuthentication = false;
   };
 
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.05";
 }
